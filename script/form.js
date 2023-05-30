fbada0b23f46a70c5e924ba084960c599dfa080c
antoniomarcuss <105389155+antoniomarcuss@users.noreply.github.com>
2023-05-30 12:14:27 -0300

    atualizando form

--- script/form.js
+++ script/form.js
@@ -3,13 +3,13 @@ let email = document.getElementById('email')
 let erro = document.querySelector('.erro')
 form.addEventListener('submit', enviarForm)
 email.addEventListener('focus', clickEmail)
-function enviarForm(){
+function enviarForm(e){
     if(!email.value){
         erro.style.display = 'block'
         email.style.backgroundColor = '#f3d4ca'
         email.classList.add('orange-placeholder')
         email.style.border = '1px solid #FF784F'
-        
+        e.preventDefault()
     }else{
         clickEmail()
     }
