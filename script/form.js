63fecbb144fb80a02d19ac28a6bd23a97d15a119
antoniomarcuss <105389155+antoniomarcuss@users.noreply.github.com>
2023-06-02 21:30:45 -0300

    melhorando o codigo

--- script/form.js
+++ script/form.js
@@ -1,28 +1,72 @@
-let form = document.querySelector('form')
-let email = document.getElementById('email')
-let erro = document.querySelector('.erro')
-let img = document.querySelector('.divisor .imgMobile2')
-form.addEventListener('submit', enviarForm)
-email.addEventListener('focus', clickEmail)
-function enviarForm(e){
-    if(!email.value){
-        erro.style.display = 'block'
-        email.style.backgroundColor = '#f3d4ca'
-        email.classList.add('orange-placeholder')
-        email.style.border = '1px solid #FF784F'
-        e.preventDefault()
+const validarEmail = function(email){
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(String(email).toLowerCase())
+}
+
+const form = document.querySelector('form')
+const emailInput = document.querySelector('#email')
+const erroEmail = document.querySelector('.erro')
+
+let isvalidForm = false
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    validarInput()
+    if(isvalidForm){
+        // POST Backend
+        form.submit()
+        console.log('validou e enviou');
     }else{
-        clickEmail()
+        console.log('não enviou');
+    }
+    
+})
+
+
+function validarInput(){
+    isvalidForm = true
+    if(!emailInput.value || !validarEmail(emailInput.value)){
+        isvalidForm = false
+        emailInput.classList.add('orange-placeholder')
+        emailInput.classList.add('borderInput')
+        erroEmail.style.display = 'block'
     }
 }
 
-function clickEmail(){
-    erro.style.display = ''
-    email.style.backgroundColor = ''
-    email.classList.remove('orange-placeholder')
-    email.style.border = ''
- 
-}
+emailInput.addEventListener('input', function(){
+    validarInput()
+    emailInput.classList.remove('orange-placeholder')
+    emailInput.classList.remove('borderInput')
+    erroEmail.style.display = 'none'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
